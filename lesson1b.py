bf86b083522526d6e8914a8e5edeb4ded3f5b89b
ck-ryan9 <catalystryan9@gmail.com>
2026-02-07 11:32:15 +0300

    added leeson1b

---
+++ lesson1b.py
@@ -0,0 +1,19 @@
+# Python Data types
+# String- It is a series of characters that are inside of quotes
+name = "Ryan Mutuku"
+print(name)
+print(type(name))
+
+# Numeric
+# Int- indicates a whole number in python. For int we dont use quotes.
+age = 18
+print("The age of a person is:" ,age)
+print(type (age))
+
+# float- This is a num with a decimal
+
+coordinates = 15.456
+print("The coordinates are:", coordinates)
+print(type(coordinates))
+
+
