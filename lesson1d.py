7071034fb6449e7d3ef134e25bc5e4a17581d8e5
ck-ryan9 <catalystryan9@gmail.com>
2026-02-07 12:04:14 +0300

    added lesson1d

---
+++ lesson1d.py
@@ -0,0 +1,6 @@
+# Create a python program that is able to calculate the simple interest given the principle as 50000, rate as 5% amd time as 8 years. SI =(p *r *t)/100
+principle = 50000
+rate = 5/100
+time = 8
+SimpleInterest = (principle * rate * time) /100
+print("The simple interest is:", SimpleInterest)
